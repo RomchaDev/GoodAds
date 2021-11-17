b8bbd4ca84db5549f7d8803c21d4661ed9fe30e1
Archi <worldofgamesprotanki@gmail.com>
2021-11-17 20:57:21 +0200

    Home finshed with bugs

--- layer-presentation/src/main/java/org/romeo/layer_presentation/main/home/HomeViewModel.kt
+++ layer-presentation/src/main/java/org/romeo/layer_presentation/main/home/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package org.romeo.layer_presentation.main.home
 
 import org.romeo.layer_domain.use_cases.GetUserAdsUseCase
+import org.romeo.layer_presentation.core.app_state.AppState
 import org.romeo.layer_presentation.core.main.BaseViewModel
 import org.romeo.layer_presentation.core.navigation.AppNavigator
 
@@ -11,7 +12,7 @@ class HomeViewModel(
 
     override fun onViewInit() {
         runAsync {
-            getUserAdsUseCase.execute()
+            mStateLiveData.postValue(AppState.Success(HomeViewState(getUserAdsUseCase.execute())))
         }
     }
 }
