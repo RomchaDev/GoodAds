afe752d1f00969d7d0aaea498db971f3ea88800f
Archi5514 <worldofgamesprotanki@gmail.com>
2021-11-27 20:40:51 +0200

    Some changes

--- layer-presentation/src/main/java/org/romeo/layer_presentation/main/requests/RequestsViewState.kt
+++ layer-presentation/src/main/java/org/romeo/layer_presentation/main/requests/RequestsViewState.kt
@@ -1,4 +1,4 @@
-package org.romeo.layer_presentation.main.ad_requests
+package org.romeo.layer_presentation.main.requests
 
 import org.romeo.layer_domain.app_state.AppStateEntity
 import org.romeo.layer_domain.entity.list.items.UserAdsListItem
