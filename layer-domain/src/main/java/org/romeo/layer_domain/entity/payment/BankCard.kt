0f977d8a7474f777ca55b8fbcdec93fe3bd7f6c8
Romeo2005 <ua.roma2005@gmail.com>
2022-01-01 14:42:43 +0200

    refactor: renamed PaymentEntity to BankCard and moved back arrow a bit down in fragment_payment.xml

--- layer-domain/src/main/java/org/romeo/layer_domain/entity/payment/BankCard.kt
+++ layer-domain/src/main/java/org/romeo/layer_domain/entity/payment/BankCard.kt
@@ -1,7 +1,8 @@
 package org.romeo.layer_domain.entity.payment
 
-data class PaymentEntity(
+data class BankCard(
     val card_number: String,
+    val cardHolderName: String,
     val expiry_date: String,
     val cvv2: String
 )
--- layer-domain/src/main/java/org/romeo/layer_domain/entity/payment/PayAdvertisingRequestEntity.kt
+++ layer-domain/src/main/java/org/romeo/layer_domain/entity/payment/PayAdvertisingRequestEntity.kt
@@ -2,5 +2,5 @@ package org.romeo.layer_domain.entity.payment
 
 data class PayAdvertisingRequestEntity(
     val requestId: String,
-    val payment: PaymentEntity
+    val card: BankCard
 )
